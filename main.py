d2231171b6361bf234913f326304a371a6e2f37b
elisei.bobocea <elisey93@icloud.com>
2022-09-22 10:38:06 +0100

    -api_id and api_key added

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+APP_ID = "aaa4dab8"
+API_KEY = "40510ed79f98e989525b951cf980f9b8"
