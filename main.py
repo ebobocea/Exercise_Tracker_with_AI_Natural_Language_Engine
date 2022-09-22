55937df9e2650384f188074df2f91b41063d0968
elisei.bobocea <elisey93@icloud.com>
2022-09-22 11:56:48 +0100

    -api_id and api_key added

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+import requests
+
 APP_ID = "aaa4dab8"
 API_KEY = "40510ed79f98e989525b951cf980f9b8"
+
+nutritionix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+nutritionix_params = {
+ "query": "ran 3 miles",
+}
+headers = {
+ "x-app-id": APP_ID,
+ "x-app-key": API_KEY,
+}
+
+response = requests.post(url=nutritionix_endpoint, json=nutritionix_params, headers=headers)
+
+print(response.text)
